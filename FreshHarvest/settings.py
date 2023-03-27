77e7511014a77acc9a259380ee474298cea86bfb
Amjad <amjadcaju@gmail.com>
2023-03-27 11:11:06 +0530

    FreshHarvest

--- FreshHarvest/settings.py
+++ FreshHarvest/settings.py
@@ -90,13 +90,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'FreshHarvest.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freshharvestdb',
+        'USER': 'amjad',
+        'PASSWORD': 'Amjad ameen',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
