64202d6ea3b6ad38ba71b6ba6e14f26313df033e
Amjad <amjadcaju@gmail.com>
2023-03-31 16:48:26 +0530

    static settings

--- FreshHarvest/settings.py
+++ FreshHarvest/settings.py
@@ -162,16 +162,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 if DEBUG == True:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR/'static'
+    STATIC_URL ='/static/'
+    STATIC_ROOT =BASE_DIR/'static'
     STATICFILES_DIRS=[
-        'FreshHarvest/static'
+        '/FreshHarvest/static/'
     ]                                                                     
 else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, '/FreshHarvest/static')
+    STATIC_URL ='/static/'
+    STATIC_ROOT ='/home/ubuntu/e-commerce/'
     STATICFILES_DIRS=[
-        'FreshHarvest/static'
+        '/FreshHarvest/static/'
     ]
 
 # Default primary key field type
