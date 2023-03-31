19302922474c79d04a4d8cd3ec3bb34b93b009e0
Amjad <amjadcaju@gmail.com>
2023-03-31 15:19:31 +0530

    static changes

--- FreshHarvest/settings.py
+++ FreshHarvest/settings.py
@@ -32,7 +32,6 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -164,12 +163,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 if DEBUG == True:
     STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR/'static'
+    STATICFILES_DIRS=[
+        'FreshHarvest/static'
+    ]                                                                     
 else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'adminPanel', 'static')
-    ]
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS=[
+        'FreshHarvest/static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
