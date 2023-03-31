513c60113385f14de512c1a671bcf2b55d7b767a
Amjad <amjadcaju@gmail.com>
2023-03-31 10:39:27 +0530

    new

--- FreshHarvest/urls.py
+++ FreshHarvest/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('admin/', include('adminPanel.urls')),
     # path('payments/', include('payments.urls')),
     
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT),
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
